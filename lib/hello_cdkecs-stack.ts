83f502f3ad6ea2735000532ce8fb15113b058b60
Junfeng Li <lijunfen@amazon.com>
2019-06-07 18:10:41 -0700

    Add fargate service infrastructure.

--- lib/hello_cdkecs-stack.ts
+++ lib/hello_cdkecs-stack.ts
@@ -1,9 +1,28 @@
-import cdk = require('@aws-cdk/cdk');
+import * as cdk from '@aws-cdk/cdk';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 
 export class HelloCdkecsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAZs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.LoadBalancedFargateService(this, 'MyFargateService', {
+      cluster: cluster,  // Required
+      cpu: '512', // Default is 256
+      desiredCount: 6,  // Default is 1
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"), // Required
+      memoryMiB: '2048',  // Default is 512
+      publicLoadBalancer: true  // Default is false
+    });
   }
 }
